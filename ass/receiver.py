2707a8308c5b8c7860220e8ce2ff7392b92b52e5
Jack Jiang <jackjyq@outlook.com>
2018-10-03 10:02:59 +1000

    add a little

--- ass/receiver.py
+++ ass/receiver.py
@@ -1,12 +1,43 @@
+# python receiver.py receiver_port file_r.pdf
+
+
+
 import socket
+import argparse
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+
+HOST = "0.0.0.0"
+BUFFER = 1024
+
+
+def get_args ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('port', type=int)
+    parser.add_argument('file', type=str)
+    args = parser.parse_args()
+    address = (HOST, args.port)
+    return args, address
+
+
+
+args, address = get_args()
+
+
+
+
+
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(address)
+print("Receiver ready!\n")
+
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print ("received message:", data.decode('utf-8'))
+    data, addr = sock.recvfrom(BUFFER)
+    if data:
+        print("File name:", data)
+        file_name = data.strip()
+
+    # print ("received message:", data.decode('utf-8'))
