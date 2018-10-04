3345010e3f5d1bacc9334376b37035cbcd27b0b4
Jack Jiang <jackjyq@outlook.com>
2018-10-04 16:33:29 +1000

    rdt2.2 not work

--- ass/receiver.py
+++ ass/receiver.py
@@ -15,53 +15,82 @@ def get_args ():
 
 
 class Receiver():
+    # sent package:
+    #   header:
+    #       sequence: 1 byte
+    #       checksum: 2 bytes
+    #   payload
+    #
+    # ack package:
+    #   sequence: 1 byte
+    #
     def __init__(self, receiver_addr, buffer_size):
         self.receiver_addr = receiver_addr
         self.buffer_size = buffer_size
         self.sender_addr = ""
         self.package = bytes()
-        self.payload = bytes()
+        self.sequence = bytes()
+        self.last_sequence = bytes()
         self.checksum = bytes()
+        self.payload = bytes()
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(receiver_addr)
         print("Receiver is listening on:", receiver_addr, "\n")
     def receive_package(self):
         self.package, self.sender_addr = self.sock.recvfrom(buffer_size)
-        self.payload = self.package[2:]
-        self.checksum = (self.package[0], self.package[1])
+        self.sequence = self.package[0]
+        self.checksum = (self.package[1], self.package[2])
+        self.payload = self.package[3:]
     def send_ACK(self):
-        self.sock.sendto(bytes([1]), self.sender_addr)
-    def send_NAK(self):
-        self.sock.sendto(bytes([0]), self.sender_addr)
+        self.sock.sendto(self.last_sequence, self.sender_addr)
+    # def send_NAK(self):
+    #     self.sock.sendto(bytes([0]), self.sender_addr)
     def close(self):
         self.sock.close()
-    def receive_good_package(self):
+    # def receive_good_package(self):
+    #     while(True):
+    #         self.receive_package()
+    #         if Checksum.validate_checksum(self.package):
+    #             print("send ACK to: ", self.sender_addr)
+    #             self.send_ACK()
+    #             break
+    #         else:
+    #             print("send NAK to: ", self.sender_addr)
+    #             self.send_NAK()
+    def receive(self):
         while(True):
+            # receive extract of the package
             self.receive_package()
-            if Checksum.validate_checksum(self.package):
+            # not currupted, update last sequence number
+            if (Checksum.validate_checksum(self.package)\
+                and (self.sequence != self.last_sequence[0])):
+                self.last_sequence = bytes([self.sequence])
                 print("send ACK to: ", self.sender_addr)
                 self.send_ACK()
                 break
-            else:
+            else:   # currupted
                 print("send NAK to: ", self.sender_addr)
-                self.send_NAK()
-
+                self.send_ACK()
 
 
 if __name__ == '__main__':
+    # get the arguments Receiver
     args = get_args()
     host = "0.0.0.0"
     buffer_size = 1024
     receiver_addr = (host, args.receiver_port)
+    # create instance of Receiver
     instance = Receiver(receiver_addr, buffer_size)
 
-    instance.receive_good_package()
+    # receiver and write
+    instance.receive()
     with open(args.file_r, 'wb') as fd:
         try:
             while(instance.package):
                 fd.write(instance.payload)
                 instance.sock.settimeout(2)
-                instance.receive_good_package()
+                instance.receive()
         except socket.timeout:
+            # close socket
             instance.close()
-            print("\nSave file", args.file_r, "from", instance.sender_addr)
+    print("\nSave file", args.file_r, "from", instance.sender_addr)
--- ass/sender.py
+++ ass/sender.py
@@ -33,17 +33,31 @@ def get_args ():
 
 
 class Sender():
+    # sent package:
+    #   header:
+    #       sequence: 1 byte
+    #       checksum: 2 bytes
+    #   payload
+    #
+    # ack package:
+    #   sequence: 1 byte
+    #
     def __init__(self, receiver_addr):
         self.receiver_addr = receiver_addr
         self.package = bytes()
-        self.payload = bytes()
+        self.sequence = bytes([0])
         self.checksum = bytes()
+        self.payload = bytes()
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        print("Socket created!\n")
+        print("Sender is ready\n")
     def make_package(self, payload):
         self.payload = payload
-        self.checksum = Checksum.calculate_checksum(payload)
-        self.package = bytes(list(self.checksum)) + self.payload
+        self.checksum = Checksum.calculate_checksum(\
+                        self.sequence\
+                        + self.payload)
+        self.package = self.sequence\
+                       + self.checksum\
+                       + self.payload
     def close(self):
         self.sock.close()
     def send_package(self):
@@ -51,20 +65,29 @@ class Sender():
         print("send package with checksum: ",
                 self.checksum[0], self.checksum[1])
         return sent_bytes
-    def send_good_package(self):
-        while(True):
-            self.send_package()
-            pkg, addr = self.sock.recvfrom(ACK_SIZE)
-            ack = pkg[0]
-            if (ack == 1):
-                print("received ACK from: ", addr)
-                break
-            else:
-                print("received NAK from: ", addr)
-                continue
-    def rdt_send(self, data):
+    def increase_sequence(self):
+        if (self.sequence == bytes([0])):
+            self.sequence == bytes([1])
+        elif (self.sequence == bytes([1])):
+            self.sequence == bytes([0])
+        else:
+            raise "Bad sequence number!"
+    def send(self, data):
         self.make_package(data)
-        self.send_good_package()
+        while(True):
+            # send and wait
+            self.send_package()
+            ack_package, ack_address = self.sock.recvfrom(ACK_SIZE)
+            # determine whether the return is currupted
+            ack_sequence = ack_package[0]
+            if (ack_sequence == self.sequence[0]):
+                # not currupted, increase sequence number
+                self.increase_sequence()
+                print("received package: ", self.sequence[0])
+                break
+            else:   # currupted, resend package
+                print("resend package: ", self.sequence[0])
+                continue
 
 
 
@@ -79,8 +102,9 @@ if __name__ == '__main__':
     with open(args.file, 'rb') as fd:
         data = fd.read(READ_SIZE)
         while (data):
-            instance.rdt_send(data)
+            instance.send(data)
             data = fd.read(READ_SIZE)
+
     # close
     instance.close()
     print("\nSend", args.file ,"to", instance.receiver_addr)
